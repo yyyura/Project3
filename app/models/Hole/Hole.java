9e8bb5e3afdc5ecce73b0bcf549898dec6561b2b
x00132764 <x00132764@gmail.com>
2017-04-02 16:38:10 +0100

    changed name of shots to score in Hole, added getter method

--- app/models/Hole/Hole.java
+++ app/models/Hole/Hole.java
@@ -7,16 +7,20 @@ public class Hole {
     private int holeNumber;
     private int par;
     private int index;      //the difficulty of the hole, rated from 1-18 (1 being the easiest hole on the course)
-    private int shots;      //the amount of shots the user took on this hole
+    private int score;      //the amount of shots the user took on this hole
 
     public Hole(int holeNumber, int par, int index) {
         this.holeNumber = holeNumber;
         this.par = par;
         this.index = index;
-        this.shots = 0;
+        this.score = 0;
     }
 
-    public void setShots(int shots){
-        this.shots = shots;
+    public void setScore(int shots){
+        this.score = shots;
+    }
+
+    public int getScore(){
+        return score;
     }
 }
