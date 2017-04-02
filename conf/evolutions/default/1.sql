97c783870d405ada8f42dee35f3698e13a2bc3ad
yyyura <yyyura@gmail.com>
2017-04-02 19:23:38 +0100

    Configuring Ebean and the database Test Mapping the Products class

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table product (
+  id                            integer not null,
+  name                          varchar(255),
+  description                   varchar(255),
+  category                      varchar(255),
+  stock                         integer,
+  price                         double,
+  constraint pk_product primary key (id)
+);
+create sequence product_seq;
+
+
+# --- !Downs
+
+drop table if exists product;
+drop sequence if exists product_seq;
+
