2427947553b0601b50f92dcb52c7d14af510304a
x00132764 <x00132764@gmail.com>
2017-04-03 18:29:17 +0100

    fixed errors with hole and course classes, renamed to distinguish from packages

--- app/models/Course/CourseClass.java
+++ app/models/Course/CourseClass.java
@@ -1,23 +1,23 @@
-package models.golf;
+package models.Course;
 
-
+import models.Hole.HoleClass;
 /**
  * Created by awhit on 3/31/2017.
  */
-public class Course{
+public class CourseClass{
     private String courseName;
-    private Hole holes[];
+    private HoleClass holes[];
     private int totalPar;
 
-    public Course(String courseName) {
+    public CourseClass(String courseName) {
         this.courseName = courseName;
-        this.holes = new Hole[18];
+        this.holes = new HoleClass[18];
         totalPar = 0;
     }
 
     public void setPar() {
         for (int i = 0; i < 18; i++) {
-            totalPar += holes[i].par;
+            totalPar += holes[i].getPar();
         }
     }
 }
--- app/models/Hole/HoleClass.java
+++ app/models/Hole/HoleClass.java
@@ -1,23 +1,28 @@
-package models.golf;
+package models.Hole;
 
 /**
  * Created by awhit on 3/31/2017.
  */
-public class Hole {
+public class HoleClass {
     private int holeNumber;
-    protected int par;
+    private int par;
     private int index;      //the difficulty of the hole, rated from 1-18 (1 being the easiest hole on the course)
     private int score;      //the amount of shots the user took on this hole
 
-    public Hole(int holeNumber, int par, int index) {
+    public HoleClass(int holeNumber, int par, int index) {
         this.holeNumber = holeNumber;
         this.par = par;
         this.index = index;
         this.score = 0;
     }
 
+    public int getPar() {
+        return par;
+    }
+
     public void setScore(int shots){
         this.score = shots;
+
     }
 
     public int getScore(){
