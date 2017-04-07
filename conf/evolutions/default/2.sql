2f37f09705a376aa1d0d00389ba002d87cb5a98b
yyyura <yyyura@gmail.com>
2017-04-07 17:34:14 +0100

    List of Holes added

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,12 @@
+# --- Sample dataset
+
+# --- !Ups
+
+
+
+insert into hole_test (id,hole_number,par,diffindex,score) values ( 1,1,4,1,7 );
+insert into hole_test (id,hole_number,par,diffindex,score) values ( 2,2,4,2,8 );
+insert into hole_test (id,hole_number,par,diffindex,score) values ( 3,3,4,3,7 );
+insert into hole_test (id,hole_number,par,diffindex,score) values ( 4,4,4,4,7 );
+
+
